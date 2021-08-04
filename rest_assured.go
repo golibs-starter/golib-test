1bdc7e7c7f45f3f473b988e3731f7511890bbc26
Thang Bui <thangbx@onemount.com>
2021-08-04 09:26:11 +0700

    Update rest assured

--- rest_assured.go
+++ rest_assured.go
@@ -4,34 +4,37 @@ import (
 	"fmt"
 	assert "github.com/stretchr/testify/require"
 	"github.com/tidwall/gjson"
-	"net/http/httptest"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
 type RestAssured struct {
-	t *testing.T
-	w *httptest.ResponseRecorder
+	t    *testing.T
+	resp *http.Response
 }
 
-func NewRestAssured(t *testing.T, w *httptest.ResponseRecorder) *RestAssured {
+func NewRestAssured(t *testing.T, resp *http.Response) *RestAssured {
 	return &RestAssured{
-		t: t,
-		w: w,
+		t:    t,
+		resp: resp,
 	}
 }
 
 func (r *RestAssured) Status(httpStatusCode int) *RestAssured {
-	assert.Equal(r.t, httpStatusCode, r.w.Code)
+	assert.Equal(r.t, httpStatusCode, r.resp.StatusCode)
 	return r
 }
 
 func (r *RestAssured) Header(key string, expected interface{}) *RestAssured {
-	assert.EqualValues(r.t, expected, r.w.Header().Get(key))
+	assert.EqualValues(r.t, expected, r.resp.Header.Get(key))
 	return r
 }
 
 func (r *RestAssured) Body(key string, expected interface{}) *RestAssured {
-	v := gjson.Get(r.w.Body.String(), key)
+	byteBody, err := ioutil.ReadAll(r.resp.Body)
+	assert.NoError(r.t, err)
+	v := gjson.Get(string(byteBody), key)
 	assert.EqualValues(r.t, expected, v.Value(), fmt.Sprintf("Expected value of key %v is %v, but got: %v", key, expected, v.Value()))
 	return r
 }
