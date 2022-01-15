96890e6b2803869a291f55d4042b880b46105a58
ndnam90 <ndnam90@gmail.com>
2022-01-16 02:15:03 +0700

    Add more functions to test suite

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.8.1
 	gitlab.com/golibs-starter/golib v0.9.14-beta
+	gitlab.com/golibs-starter/golib-message-bus v0.1.5-beta // indirect
 	go.uber.org/fx v1.13.1
 	gorm.io/gorm v1.22.3
 )
--- test_suite.go
+++ test_suite.go
@@ -3,17 +3,24 @@ package golibtest
 import (
 	"crypto/rsa"
 	"fmt"
+	"github.com/Shopify/sarama"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/golibs-starter/golib-message-bus/kafka/properties"
 	"gitlab.com/golibs-starter/golib/web/log"
 	"gorm.io/gorm"
+	"testing"
 	"time"
 )
 
+// TestSuite represent testing suite
 type TestSuite struct {
-	DB         *gorm.DB
-	jwtSignKey *rsa.PrivateKey
+	DB              *gorm.DB
+	KafkaProperties *properties.Client
+	jwtSignKey      *rsa.PrivateKey
 }
 
+// CreateJwtToken return a new jwt token
 func (ts *TestSuite) CreateJwtToken(userId string) string {
 	token := jwt.New(jwt.GetSigningMethod("RS256"))
 	now := time.Now()
@@ -30,6 +37,7 @@ func (ts *TestSuite) CreateJwtToken(userId string) string {
 	return jwtToken
 }
 
+// LoadJwtPrivateKey load jwt config from properties
 func (ts *TestSuite) LoadJwtPrivateKey(jwtProperties *JwtTestProperties) {
 	signKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(jwtProperties.PrivateKey))
 	if err != nil {
@@ -38,6 +46,7 @@ func (ts *TestSuite) LoadJwtPrivateKey(jwtProperties *JwtTestProperties) {
 	ts.jwtSignKey = signKey
 }
 
+// TruncateTables run truncate statement
 func (ts *TestSuite) TruncateTables(tables []string) {
 	for _, table := range tables {
 		if err := ts.DB.Exec(fmt.Sprintf("TRUNCATE TABLE `%s`", table)).Error; err != nil {
@@ -46,8 +55,44 @@ func (ts *TestSuite) TruncateTables(tables []string) {
 	}
 }
 
+// Seed insert data to database
 func (ts *TestSuite) Seed(model interface{}) {
 	if err := ts.DB.Create(model).Error; err != nil {
 		log.Fatalf("Could not create seed data, model: %v, err: %v")
 	}
+}
+
+// AssertDatabaseCount assert database has number of row without query
+func (ts *TestSuite) AssertDatabaseCount(t *testing.T, table string, expected int64) {
+	var count int64
+	ts.DB.Table(table).Count(&count)
+	require.Equal(t, expected, count)
+}
+
+// AssertDatabaseHas assert database has more than a row with query params
+func (ts *TestSuite) AssertDatabaseHas(t *testing.T, table string, conditions map[string]interface{}) {
+	var count int64
+	ts.DB.Table(table).Where(conditions).Count(&count)
+	require.GreaterOrEqual(t, count, int64(1), "Record not found in database with query:", conditions)
+}
+
+// DeleteKafkaTopics delete topics kafka
+func (ts *TestSuite) DeleteKafkaTopics() {
+	config := sarama.NewConfig()
+	config.Version = sarama.V1_1_0_0
+	clusterAdmin, err := sarama.NewClusterAdmin(ts.KafkaProperties.BootstrapServers, config)
+	if err != nil {
+		log.Fatalf("Could not create sarama cluster admin: %v", err)
+	}
+	defer clusterAdmin.Close()
+	topics, err := clusterAdmin.ListTopics()
+	if err != nil {
+		log.Fatalf("Could not list kafka topics", err)
+	}
+	for topic, _ := range topics {
+		if err := clusterAdmin.DeleteTopic(topic); err != nil && err != sarama.ErrUnknownTopicOrPartition {
+			log.Fatalf("Could not delete kafka topic", err)
+		}
+	}
+	clusterAdmin.ListTopics()
 }
