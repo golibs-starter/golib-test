28f12a06e9c2ee7612b16bde826d191609b49320
Thang Bui <zenthangplus@gmail.com>
2022-09-29 15:35:25 +0700

    Add WaitUntilT function

--- utils.go
+++ utils.go
@@ -2,6 +2,8 @@ package golibtest
 
 import (
 	"context"
+	assert "github.com/stretchr/testify/require"
+	"testing"
 	"time"
 )
 
@@ -10,6 +12,21 @@ func WaitUntil(cb func() bool, timeout time.Duration) {
 	defer cancel()
 	for {
 		if ctx.Err() != nil || cb() {
+			break
+		}
+	}
+}
+
+func WaitUntilT(t testing.TB, cb func() bool, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	for {
+		if ctx.Err() != nil {
+			assert.FailNowf(t, "Condition not matched",
+				"Condition not matched with timeout [%s], err [%v]", timeout.String(), ctx.Err())
+			break
+		}
+		if cb() {
 			break
 		}
 	}
